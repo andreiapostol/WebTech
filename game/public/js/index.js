d09c39ba12bbb492e272997c9ad5dea17978ed26
Andrei-Laurentiu Apostol <andreiapostol@eduroam-194-227.nomadic.bris.ac.uk>
2019-02-11 20:09:50 +0000

    Small progress

--- game/public/js/index.js
+++ game/public/js/index.js
@@ -1,4 +1,26 @@
+function loadImage(url){
+    return new Promise(resolve => {
+        const image = new Image();
+        image.addEventListener('load', ()=>{
+            resolve(image);
+        });
+        image.src = url;
+    })
+}
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
-context.fillRect(25, 0, 50, 90);
+// context.fillRect(25, 0, 50, 90);
+
+loadImage('/img/inca_back2.png').then(image => {
+    context.drawImage(image,
+        0, 0,
+        32, 32, 
+        0, 0, 
+        32, 32);
+    context.drawImage(image,
+            96, 32,
+            32, 32, 
+            0, 32, 
+            32, 32);
+});
