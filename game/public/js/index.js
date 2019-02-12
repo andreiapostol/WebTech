4c227b65273a4eab4d26c56e646941e7db29b97f
Marc Tannous <work@MacBook-Pro-de-Marc.local>
2019-02-12 00:52:42 +0000

    level layout and character sprite

--- game/public/js/index.js
+++ game/public/js/index.js
@@ -1,26 +1,46 @@
-function loadImage(url){
-    return new Promise(resolve => {
-        const image = new Image();
-        image.addEventListener('load', ()=>{
-            resolve(image);
-        });
-        image.src = url;
+import {imageLoader, mapLoader} from './loaders.js';
+import SpriteAPI from './spriteAPI.js';
+
+function drawConstant(background, context, sprites){
+    background.intervals.forEach(([x1, x2, y1, y2]) => {
+        for(let x = x1; x < x2; x++){
+            for(let y = y1; y < y2; y++){
+                sprites.drawTile(background.tile, context, x, y);
+            }
+        }
     })
 }
+
+function drawAlternative(background, context, sprites){
+    background.intervals.forEach(([x1, x2, y1, y2]) => {
+        let step = 1;
+        for(let x = x1; x < x2; x++){
+            for(let y = y1; y < y2; y++){
+                step === 1 ? sprites.drawTile(background.tileA, context, x, y) : sprites.drawTile(background.tileB, context, x, y);
+                step *= -1;
+            }
+        }
+    })
+}
+
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
-// context.fillRect(25, 0, 50, 90);
+imageLoader('/img/inca_back.png').then(image => {
+    const sprites = new SpriteAPI(image, 16, 16);
+    sprites.define('background', 7, 13);
+    sprites.define('groundA', 2, 4);
+    sprites.define('groundB', 3, 4);
+    sprites.define('underground', 2, 11);
 
-loadImage('/img/inca_back2.png').then(image => {
-    context.drawImage(image,
-        0, 0,
-        32, 32, 
-        0, 0, 
-        32, 32);
-    context.drawImage(image,
-            96, 32,
-            32, 32, 
-            0, 32, 
-            32, 32);
+    mapLoader('intro').then(level => {
+        // bg
+        drawConstant(level.backgrounds[0], context, sprites);
+        // floor
+        drawAlternative(level.backgrounds[1], context, sprites);
+        // beneath the floor
+        drawConstant(level.backgrounds[2], context, sprites);
+    });
+
+
 });
--- readme.txt
+++ readme.txt
@@ -1,4 +1,4 @@
 aa16366 - Andrei Apostol
 mt16435 - Marc Tannous
 
-We would want to build a browser game using HTML5's canvas, thus writing JS algorithms for interesting problems such as motion (gravity) and object collision. An interesting approach would be to use retro spritesheets for characters and tiles.
+We would want to build a browser game using HTML5's canvas, thus writing JS algorithms for interesting problems such as motion (gravity) and object collision. An interesting approach would be to use retro spritesheets for characters and tiles. 
