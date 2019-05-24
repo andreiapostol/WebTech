fa045876d2f4faa58386fb49726efcffed08c4dd
Marc Tannous <tannous.marc@gmail.com>
2019-05-24 18:07:53 +0100

    latest map is at the top

--- client/public/js/maps.js
+++ client/public/js/maps.js
@@ -1,5 +1,5 @@
 const maps = JSON.parse(localStorage.getItem("recentMaps"));
-maps.forEach(map => {
+maps.reverse().forEach(map => {
     let tr = document.createElement("tr");
     let td1 = document.createElement("td");
     let td2 = document.createElement("td");
