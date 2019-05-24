bc7159ad681c811ce1a92ff45674ea9cc1c7946a
Marc Tannous <tannous.marc@gmail.com>
2019-05-24 17:56:50 +0100

    seed url in maps

--- client/public/js/maps.js
+++ client/public/js/maps.js
@@ -5,9 +5,8 @@ maps.forEach(map => {
     let td2 = document.createElement("td");
     let td3 = document.createElement("td");
     td3.insertAdjacentHTML('beforeend', '    <fieldset class="star-rating"><div class="star-rating__stars"><input class="star-rating__input" type="radio" name="rating" value="1" id="rating-1" /><label class="star-rating__label" for="rating-1" aria-label="One"></label><input class="star-rating__input" type="radio" name="rating" value="2" id="rating-2" /><label class="star-rating__label" for="rating-2" aria-label="Two"></label><input class="star-rating__input" type="radio" name="rating" value="3" id="rating-3" /><label class="star-rating__label" for="rating-3" aria-label="Three"></label>                <input class="star-rating__input" type="radio" name="rating" value="4" id="rating-4" /><label class="star-rating__label" for="rating-4" aria-label="Four"></label><input class="star-rating__input" type="radio" name="rating" value="5" id="rating-5" /><label class="star-rating__label" for="rating-5" aria-label="Five"></label></div></fieldset>');
-    let seed = document.createTextNode(map.seed);
+    td1.insertAdjacentHTML('beforeend', '<a href="./index.html?seed=' + map.seed + '">' + map.seed + "</a>");
     let time = document.createTextNode(map.time);
-    td1.appendChild(seed);
     td2.appendChild(time);
     tr.appendChild(td1);
     tr.appendChild(td2);
